f0ec90e0bfaf33904d600201a97d232b7853ebbc
dkMiMuKr <mikkel.munk.kristensen@lego.com>
2024-10-23 21:45:15 +0200

    tab functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
-  const key = "demopad";
-  const [text, setText] = useState(() => localStorage.getItem(key) || "");
+  const textAreaElement = useRef<HTMLTextAreaElement>(null);
+  const storageKey = "demopad";
+
+  useEffect(() => textAreaElement.current?.focus(), []);
+  const handleTab = useCallback((e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    if (e.key === "Tab") {
+      e.preventDefault();
+      const start = e.currentTarget.selectionStart;
+      const end = e.currentTarget.selectionEnd;
+      e.currentTarget.value = e.currentTarget.value.substring(0, start) + "    " + e.currentTarget.value.substring(end);
+      e.currentTarget.selectionStart = e.currentTarget.selectionEnd = start + 4;
+    }
+  }, []);
+
   return (
     <textarea
-      value={text}
-      onChange={(e) => {
-        localStorage.setItem(key, e.target.value);
-        setText(e.target.value);
-      }}
+      ref={textAreaElement}
+      defaultValue={localStorage.getItem(storageKey) || ""}
+      onChange={(e) => localStorage.setItem(storageKey, e.target.value)}
+      onKeyDown={handleTab}
       className="pad"
     />
   );
