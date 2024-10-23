11deb9ebc29a6bfe2684c8d0c287baf1ed0f53e6
dkMiMuKr <mikkel.munk.kristensen@lego.com>
2024-10-23 19:48:36 +0200

    save to localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <textarea className="pad" />;
+  const key = "demopad";
+  const [text, setText] = useState(() => localStorage.getItem(key) || "");
+  return (
+    <textarea
+      value={text}
+      onChange={(e) => {
+        localStorage.setItem(key, e.target.value);
+        setText(e.target.value);
+      }}
+      className="pad"
+    />
+  );
 }
 
 export default App;
